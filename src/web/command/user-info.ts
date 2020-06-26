48c86bc49d8e809ed29d2d2de93207d08d571677
lamlv <lam.luongvan2305@gmail.com>
2020-06-26 16:28:25 +0700

    add getuserinfo function

---
+++ src/web/command/user-info.ts
@@ -0,0 +1,17 @@
+import {controller} from '../controller';
+import {v4 as uuid} from 'uuid';
+import {GetUserInfoChannel, UserInfo} from '../../types';
+
+export const getUserInfo = async (): Promise<UserInfo> => {
+  const data = await controller.request({
+    id: uuid(),
+    channel: GetUserInfoChannel,
+  });
+
+  const tmp = data.data as UserInfo;
+  if (tmp) {
+    return tmp;
+  } else {
+    throw new Error("Can't get device info");
+  }
+};
